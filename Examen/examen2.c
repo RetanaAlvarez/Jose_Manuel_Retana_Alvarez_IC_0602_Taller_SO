220acf06443d935a39a0c27b49d03dd059032020
Jose Manuel Retana Alvarez <chepe_jm10@hotmail.com>
2021-05-19 12:43:30 -0500

    modificaciones y borrar archivo inecesario (examen2)

--- Examen/examen2.c
+++ Examen/examen2.c
@@ -1,25 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>   
 
-
 int main()
 {
-	char arraychar[5];
-	char letra = 'a';
-	char* punteroChar;
-	char* punteroCharDos;
-    for (arraychar[5] = 0; arraychar[5] < 5; ++arraychar[5])
-    {
-    	do{
+	int i=0;
+	char arraychar[i];
+	char letra='a';
+	char* punteroChar=arraychar;
+	char* punteroCharDos=arraychar;
 
-    	printf("arraychar [%i]: %s \n",arraychar[5] ,&letra );
+	
+    for (i; i < 5; ++i)
+    {    	
+    	printf("primer elemento de arraychar [%i]: %s \n",i ,&letra );
+    letra++;
+    }
+    printf("primer elemento de punteroChar  %s\n",punteroChar  );
+    printf("primer elemento de punteroCharDos  %s\n", punteroCharDos );
+    printf("primer elemento de punteroChar  %s\n", punteroChar  );
+    printf("quinto elemento del array  %s\n" ,&arraychar[4]  );
+    
+    	/*for (arraychar[5] ; arraychar[5] < 1; ++arraychar[5])
+    {
+    	
     	letra++;
-    	printf("imprimir\n");
-    	}
     }
 
-
-    
-
+	printf("primer  elemento de arraychar :%c\n",arraychar[1]);
+    printf("primer elemento del punteroChar%s\n",punteroChar );
+    printf("primer elemento del punteroCharDos%s\n",punteroCharDos );
+    printf("quinto elemento del punteroChar%s\n",punteroChar );
+*/
     return 0;
 }
+
+
+ // ----> Crear un puntero hacia un <<char>> de nombre "punteroCharDos" e
+	// inicializarlo con la dirección del primer elemento (index) del array "arrayChar".
+
+//----> Imprimir el contenido del quinto elemento del array "arrayChar"
+// ----> Imprimir el contenido del quinto elemento del puntero "punteroChar" con aritmética de punteros
+// ----> Imprimir el contenido del quinto elemento del puntero "punteroCharDos" con aritmética de punteros
+// ----> Imprimir el contenido de todos los elementos del puntero "punteroCharDos" con aritmética de punteros
+// usar una Estructura Repetitiva
+// ----> Imprimir el contenido de todos los elementos del puntero "punteroCharDos" con aritmética de punteros
+// usar una Estructura Repetitiva*/
